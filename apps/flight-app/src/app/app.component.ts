8845bcfc76a39ce478ba7ce3fcf7ec826045ca9d
Florian Tischler <Florian.Tischler@gmx.net>
2022-07-25 16:25:18 +0200

    chore(flight-app): refactor easteregg (#15)

--- apps/flight-app/src/app/app.component.ts
+++ apps/flight-app/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  Inject,
   OnDestroy,
   OnInit,
 } from '@angular/core';
 import { getQueryParam } from '@testing-workshop/client-shared/util/common';
 import { skipFalsy } from '@testing-workshop/shared/util/common';
 import { Subject, takeUntil } from 'rxjs';
+import { WINDOW } from '@ng-web-apis/common';
 
 @Component({
   selector: 'flight-app',
@@ -18,10 +20,14 @@ export class AppComponent implements OnInit, OnDestroy {
   private easterEgg$ = getQueryParam('easteregg');
   private destroy$$ = new Subject<void>();
 
+  constructor(@Inject(WINDOW) private window: Window) {}
+
   ngOnInit(): void {
     this.easterEgg$
       .pipe(skipFalsy(), takeUntil(this.destroy$$))
-      .subscribe(() => alert('Congrats! You have found an easter egg!'));
+      .subscribe(() =>
+        this.window.alert('Congrats! You have found an easter egg!')
+      );
   }
 
   ngOnDestroy(): void {
