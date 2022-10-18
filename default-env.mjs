688c6773b27da31dfe2da00a725525487d8b7ee0
Florian Tischler <Florian.Tischler@gmx.net>
2022-10-18 15:55:25 +0200

    chore(repo): refactor default-env.mjs

--- default-env.mjs
+++ default-env.mjs
@@ -3,14 +3,21 @@ import { join } from 'path';
 
 const { log } = console;
 
-const defaultEnv = `
-TZ=UTC
-`;
+const defaultEnv = {
+  TZ: 'UTC',
+};
 
 try {
-  writeFileSync(join(process.cwd(), '.env'), defaultEnv.trim().concat('\n'), {
-    flag: 'wx',
-  });
+  writeFileSync(
+    join(process.cwd(), '.env'),
+    Object.entries(defaultEnv)
+      .map(([k, v]) => `${k}=${v}`)
+      .join('\n')
+      .concat('\n'),
+    {
+      flag: 'wx',
+    }
+  );
   log('New .env file written');
 } catch (e) {
   log('No .env written because it already existed');
